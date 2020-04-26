2e348667cc05b5ddcfbaf42560338c6c9f792577
Vlad <nahabavladislav@gmail.com>
2020-04-26 21:49:07 +0300

    Safe routes, History view

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,6 +25,7 @@
 <script>
 import Navbar from "../components/app/Navbar";
 import Sidebar from "../components/app/Sidebar";
+import messages from "../utils/messages";
 export default {
   name: "MainLayout",
   components: { Navbar, Sidebar },
@@ -37,6 +38,16 @@ export default {
       await this.$store.dispatch('fetchInfo')
     }
     this.loading = false;
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Что-то пошло не так');
+    }
   }
 }
 </script>
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,6 +1,6 @@
 export default {
   'logout': 'Вы вышли из системы',
-  'login': 'Вы вошли в систему',
+  'login': 'Для начала войдите в систему',
   'auth/user-not-found': 'Пользователя с таким e-mail не существует',
   'auth/wrong-password': 'Неверный пароль',
   'auth/too-many-requests': 'Слишком много запросов. Попробуйте позже',
